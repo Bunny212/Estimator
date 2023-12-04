f226afb10e5a035613bc6643e70d439407b17c23
Fahad Shabbir <fahad.shabbir@esparksinc.com>
2023-12-04 18:35:26 +0500

    icon issue resolve

--- src/components/CreateInvoice.js
+++ src/components/CreateInvoice.js
@@ -586,12 +586,12 @@ const CreateInvoice = () => {
 
 
 
-                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:text-xs" >Submit</button>
+                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-lg text-sm w-full sm:w-auto text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:text-xs" >Submit</button>
 
                 </form>
                 <>
 
-
+                                            
                     {/* 
 <div class="fixed bottom-0 w-full bg-white border-t border-gray-200 flex justify-around items-center p-4">
     <div class="flex flex-col items-center">
