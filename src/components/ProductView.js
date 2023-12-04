6948db17e9918e9b73f32f2ac35889a4ccfb2037
Fahad Shabbir <fahad.shabbir@esparksinc.com>
2023-12-04 11:00:13 +0500

    icon issue resolve

--- src/components/ProductView.js
+++ src/components/ProductView.js
@@ -20,7 +20,7 @@ const customStyles = {
     bottom: 'auto',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
-    width: '90%', // Adjust width for mobile feel
+    width: '50%', // Adjust width for mobile feel
     maxWidth: '100%', // Set a maximum width for larger screens
     // borderRadius: '8px', // Rounded corners
     // boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', // Shadow for depth
@@ -323,7 +323,7 @@ function ProductView() {
       >
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           <h6 ref={(_subtitle) => (subtitle = _subtitle)}>Your Invoice Estimator Pdf is Generated. Download Your Invoice Pdf.</h6>
-            <svg onClick={closeModal} class="w-10 h-10 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <svg onClick={closeModal} class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
           </svg>
 
