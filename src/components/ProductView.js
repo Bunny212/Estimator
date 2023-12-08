c0b022f01aa74e5df9a8bfe845bc34d45bf120d5
Fahad Shabbir <fahad.shabbir@esparksinc.com>
2023-12-08 17:27:58 +0500

    refresh issue

--- src/components/ProductView.js
+++ src/components/ProductView.js
@@ -55,7 +55,7 @@ function ProductView() {
 
   useEffect(() => {
     refetch();
-  });
+  },[]);
   // const navigate = useNavigate();
 
   const [viewdata, SetViewData] = useState('No Response yet')
@@ -383,9 +383,9 @@ function ProductView() {
   //   refetch();
   // },[]);
 
-  useEffect(() => {
-    refetch();
-  }, []); 
+  // useEffect(() => {
+  //   refetch();
+  // }, []); 
 
 
   
