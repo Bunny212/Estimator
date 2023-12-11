025f4d99250a6573161d14c363ef38029571a9a9
Fahad Shabbir <fahad.shabbir@esparksinc.com>
2023-12-11 15:49:58 +0500

    url issue resolve

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
+    replaceColors: {
+      blue: 'pink',
+    },
   },
   
   plugins: [require('flowbite/plugin')],
